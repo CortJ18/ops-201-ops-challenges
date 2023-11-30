3ee58ad16d36f8b25bbf9754121c04337da690ef
Joaquin Cortes <cort8536@gmail.com>
2023-11-30 02:43:38 -0500

    my first ops challenge completion

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # ops-201-ops-challenges
 My 201 Ops challenge Repo
-My first ACP workflow
---
+++ my-first-ops-challenge/helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  helloworld
+# Author:                       Joaquin Cortes
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+
+echo "Hello World"
+
+# End
